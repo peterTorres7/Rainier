c271bd2c81346ec318c32ccd4e6e79cc4f6782aa
Annelisebx <50610153+Annelisebx@users.noreply.github.com>
2021-01-26 23:57:50 -0700

    Update App.js

--- homePage/client/src/App.js
+++ homePage/client/src/App.js
@@ -4,6 +4,8 @@ import HeaderNav from './components/HeaderNav';
 import PopularItemGroup from './components/PopularItemGroup';
 import Footer from './components/Footer';
 import CategoryNav from './components/CategoryNav';
+import Details from './components/Details'
+import ItemImage from './components/ItemImage';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <CategoryNav></CategoryNav>
         <PopularItemGroup></PopularItemGroup>
         <PopularItemGroup></PopularItemGroup>
+        <ItemImage></ItemImage>
+        <Details></Details>
         <Footer></Footer>
       </header>
     </div>

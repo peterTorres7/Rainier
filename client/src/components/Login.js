5aa59e05b9216e446e164b849e981773eebe8e81
Jason Panzer <jason.panzer@nordstrom.com>
2021-02-08 17:27:29 -0800

    framework for login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,21 +1,21 @@
 import './Login.css';
-
+import { Button, Grid, TextField } from '@material-ui/core';
+ 
 /**
- * 1. Text field for User name
- * 2. Text field for Password
  * 3. Button for Login
  * 4. Button for Signup
  * 5. Button for forgot password
+ * 6. Layout elements in form
  */
 export default function Login() {
   return (
-    <div className="login">
-      <textarea> User Name </textarea>
-      <input> Password </input>
-      <h2> Login </h2>
-      <h2> Signup </h2>
-      <p> forget your password </p>
-    </div>
+    <form className="login">
+      <TextField label="Username"/>
+      <TextField label="Password"/>
+      <Button> Login </Button>
+      <Button> Sign Up </Button>
+      <Button> Forgot your password? </Button>
+    </form>
   );
 }
 

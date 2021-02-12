07bff05420a43225a7a5080dd737e0c689aca525
Annelisebx <50610153+Annelisebx@users.noreply.github.com>
2021-02-12 11:45:26 -0800

    basic routes update

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,42 @@
 import './App.css';
 import Header from './components/Header';
 import HeaderNav from './components/HeaderNav';
-import PopularItem from './components/PopularItem';
 import PopularItemGroup from './components/PopularItemGroup';
-import Footer from './components/Footer';
-import CategoryNav from './components/CategoryNav';
+//import Footer from './components/Footer';
+//import CategoryNav from './components/CategoryNav';
 import Login from './components/Login';
 import Details from './components/Details';
 import ProfilePage from './components/ProfilePage';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  //Link
+} from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Header></Header>
-        <HeaderNav></HeaderNav>
-        <CategoryNav></CategoryNav>
-        <PopularItemGroup></PopularItemGroup>
-        <PopularItemGroup></PopularItemGroup>
-        <Footer></Footer>
-
-        <Login/>
-
-
-        <Details/>
-        <ProfilePage/>
-
-
-
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <HeaderNav/>
+      <Switch>
+        <Route path="/item/:id">
+          <Details></Details>
+        </Route>
+        <Route path="/users">
+          <ProfilePage></ProfilePage>
+        </Route>
+        <Route path="/login">
+          <Login/>
+        </Route>
+        <Route path="/">
+          <PopularItemGroup></PopularItemGroup>
+          <PopularItemGroup></PopularItemGroup>
+        </Route>
+        
+      </Switch>
+    </BrowserRouter>
   );
 }
 

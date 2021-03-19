9c597e82740d6ee631d53048c9059149f00b3f04
Annelisebx <50610153+Annelisebx@users.noreply.github.com>
2021-03-19 15:19:00 -0700

    profile link and message link

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import { Container, Button, Link } from '@material-ui/core';
+import { Container, Button } from '@material-ui/core';
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
